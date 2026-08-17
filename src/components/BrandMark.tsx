222adf30ec9c6ffba8a8f7c8ae5c6e0d3f015a28
coconutcartel <jonathan9294@gmail.com>
2026-08-17 16:33:26 +0530

    Add rehome house-and-heart brand mark

---
+++ src/components/BrandMark.tsx
@@ -0,0 +1,10 @@
+export default function BrandMark() {
+  return (
+    <span className="brand-mark" aria-hidden="true">
+      <svg viewBox="0 0 40 40" role="img">
+        <path d="M5.5 18.2 20 6.5l14.5 11.7v14.3a2.7 2.7 0 0 1-2.7 2.7H8.2a2.7 2.7 0 0 1-2.7-2.7V18.2Z" fill="currentColor"/>
+        <path d="M20 29.2s-7.1-4.1-7.1-9.1c0-2.5 1.7-4.4 4-4.4 1.4 0 2.5.7 3.1 1.8.7-1.1 1.8-1.8 3.2-1.8 2.3 0 4 1.9 4 4.4 0 5-7.2 9.1-7.2 9.1Z" fill="#fff3df"/>
+      </svg>
+    </span>
+  )
+}
